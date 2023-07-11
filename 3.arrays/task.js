c23aa17f1f5814b42e814538fd59eaeb53ec78e1
Andrey <fredperm@mail.ru>
2023-07-11 19:20:35 +0500

    arrays decided

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let filterGender = users.filter(user => user.gender === gender);
+  let mapAge = filterGender.map(user => user.age);
+  if (!users.length) {
+    return 0;
+  } else if (!filterGender.length) {
+    return 0;
+  } else return mapAge.reduce((sumAge, ageValue) => sumAge + ageValue, 0) / mapAge.length;
 }
