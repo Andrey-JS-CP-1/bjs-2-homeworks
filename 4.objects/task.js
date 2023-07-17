80adf7d6cd9c122b91c8cf98e3ff7af8ca7c7e8e
Andrey <fredperm@mail.ru>
2023-07-17 19:04:42 +0500

    objects decided

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
+let StudentFirst = new Student('Игорь', 'мужской', 26);
+let StudentSecond = new Student('Василий', 'мужской', 27);
+let StudentThird = new Student('Анна', 'женский', 25);
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (!this.marks) {
+    return;
+  }
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let mark of this.marks) {
+    sum += mark;
+  }
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
