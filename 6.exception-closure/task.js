8f0574ff080f5187fc26f930d6836954f5de5e34
Andrey <fredperm@mail.ru>
2023-08-06 21:36:02 +0500

    exception-closure decided

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+  if (Number.isNaN(Number.parseFloat(value))) { 
+    throw new Error ('Невалидное значение');
+  }
+  return parseFloat(Number(value));
+}
+    
+function validateCount(value) {
+  try {
+    return parseCount(value);  
+  } catch (Error) {
+    return Error;
+  }
+}
+
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || b + c < a || c + a < b) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+    get perimeter() {
+        return Number(this.a + this.b + this.c);
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        let area = Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+        return area;
+    }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует"
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует"
+      }
+    }
+  }
+}
